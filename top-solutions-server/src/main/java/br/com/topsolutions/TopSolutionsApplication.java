d4d0c0d921d66e339fbf0460e97b43352cb28c52
T. J. Silva <thiago.silva@imd.ufrn.br>
2026-03-31 15:54:50 -0300

    chore: adicionar ponto de entrada da aplicação spring boot
    Cria ToprecrutaApplication com @SpringBootApplication, habilitando
    auto-configuração, component scan e configuração de beans do Spring.
    Inicializa o contexto e sobe o Tomcat embutido na porta configurada

---
+++ top-solutions-server/src/main/java/br/com/topsolutions/TopSolutionsApplication.java
@@ -0,0 +1,11 @@
+package br.com.topsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopSolutionsApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TopSolutionsApplication.class, args);
+  }
+}
---
+++ top-solutions-server/src/test/java/br/com/topsolutions/toprecruta/TopSolutionsApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.topsolutions.toprecruta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TopSolutionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
